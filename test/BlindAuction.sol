2e3e1e76c165b4fb4acb50bffbbf052e60230da5
mrlinjinhai <ljh18304095936@163.com>
2025-04-02 17:19:12 +0800

    add

--- test/BlindAuction.sol
+++ test/BlindAuction.sol
@@ -67,6 +67,67 @@ contract BlindAuction {
         beneficiary.transfer(highestBid);
     }
 
+    /// 提取被超出出价的出价。
+    function withdraw() external {
+        uint amount = pendingReturns[msg.sender];
+        if(amount > 0){
+            // 将其设置为零是重要的，
+            // 因为，作为接收调用的一部分，
+            // 接收者可以在 `transfer` 返回之前重新调用该函数。（可查看上面关于“条件 -> 生效 -> 交互”的标注）
+            pendingReturns[msg.sender] = 0;
+            payable(msg.sender).transfer(amount);
+        }
+    }
+
+    // 这是一个“内部”函数，这意味着它只能从合约本身（或从派生合约）调用。
+    // 退款给之前的最高出价者。取代最高者出价
+    function placeBid(address bidder,uint value) internal returns (bool success) {
+        if(value < highestBid){
+           return false;
+        }
+        if(highestBidder != address(0)){
+           pendingReturns[highestBidder] += highestBid; 
+        }
+        highestBidder = bidder;
+        highestBid = value;
+        return true;
+    }
+
+     /// 揭示盲出价。
+     /// 将获得所有正确盲出的无效出价的退款，以及除了最高出价之外的所有出价。
+    function reveal(
+        uint[] calldata values, 
+        bool[] calldata fakes, 
+        bytes32[] calldata secrets) 
+        external 
+        onlyAfter(biddingEnd) 
+        onlyBefore(revealEnd){
+        //参数校验
+        uint length = bids[msg.sender].length;
+        require(values.length == length);
+        require(values.length == length);
+        require(values.length == length);
+
+        uint refund = 0;
+        for (uint i=0; i < length; i++) 
+        {
+            Bid storage bindToCheck = bids[msg.sender][i];
+            (uint value, bool fake, bytes32 secret) = (values[i], fakes[i], secrets[i]);
+            if(bindToCheck.blindedBid != keccak256(abi.encodePacked(value,fake,secret))){
+                continue;
+            } 
+            refund += value;
+            if(!fake && bindToCheck.deposit >= value){
+                 if(placeBid(msg.sender, value)){
+                    refund -= value;
+                 }
+            } 
+            // 使发送者无法重新取回相同的存款
+            bindToCheck.blindedBid = bytes32(0);
+        }
+         
+    }
+
     /**
     * @dev 生成盲拍出价的哈希承诺
     * @param value 出价金额（单位：wei）
